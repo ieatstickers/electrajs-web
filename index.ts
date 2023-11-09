d7c88a11c91655374ec4e438f54985a46797b0a4
mikeburke <mikeburke.214@gmail.com>
2023-11-09 20:42:59 +0000

    - Exported ResponseHeaders

--- index.ts
+++ index.ts
@@ -6,3 +6,4 @@ export { Response } from "./src/Response/Response";
 export { ResponseInterface } from "./src/Response/ResponseInterface";
 export { ParsedQueryParam } from "./src/Request/Type/ParsedQueryParam";
 export { ParsedQueryParams } from "./src/Request/Type/ParsedQueryParams";
+export { ResponseHeaders } from "./src/Response/Type/ResponseHeaders";
