dade4528350f6948405650669beca164ecea2647
mikeburke <mikeburke.214@gmail.com>
2023-12-04 20:12:39 +0000

    - Build separate bundles for commonjs and es modules - Exclude all npm packages from webpack bundles

--- dist/src/Type/ParsedQueryParam.d.ts
+++
@@ -1,2 +0,0 @@
-import { ParsedQueryParams } from "./ParsedQueryParams";
-export type ParsedQueryParam = string | Array<string> | ParsedQueryParams | ParsedQueryParams[];
--- dist/src/Type/ParsedQueryParams.d.ts
+++
@@ -1,4 +0,0 @@
-import { ParsedQueryParam } from "./ParsedQueryParam";
-export interface ParsedQueryParams {
-    [key: string]: ParsedQueryParam;
-}
